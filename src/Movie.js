f97af24093b325feee6e129bbf227cd7571c42cb
Heui Beom Park <>
2022-02-19 16:22:51 +0900

    add css style

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import './Movie.css';
 
-function Movie( { id, title, year, summary, poster}) {
+function Movie( { id, title, year, summary, poster, genres}) {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
-                <h3 className="movie__title" style={{ backgroundColor: 'red' }}>{title}</h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => {
+                        return <li key={index} className="movie_genre">{genre}</li>
+                    })}
+                </ul>
                 <p className="movie__summary">{summary}</p>
             </div>
         </div>
@@ -19,6 +25,7 @@ Movie.prototype = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
